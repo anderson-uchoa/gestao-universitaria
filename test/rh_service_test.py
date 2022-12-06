d368738e909cfb52c4beac85a24033a8d46c5cf8
Anderson <andersonguchoa@gmail.com>
2022-12-06 08:40:28 -0300

    fix test file

--- test/rh_service_test.py
+++ test/rh_service_test.py
@@ -73,7 +73,7 @@ class RHServiceCase(unittest.TestCase):
         self.assertTrue(self.rh.cadastrar(self.tercAdriana), "O terceirizado deveria ter sido adicionado")
 
     def test_removerFuncionario(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertTrue(self.rh.remover(self.cpfLacerda), "Deve ser possivel remover funcionario cadastrado")
         self.assertTrue(self.rh.remover(self.cpfAdriana), "Deve ser possivel remover funcionario cadastrado")
@@ -81,33 +81,33 @@ class RHServiceCase(unittest.TestCase):
         self.assertFalse(self.rh.remover(self.cpfNulo), "Nao e possivel remover um usuario nao cadastrado")
 
     def test_removerFuncionarioDuasVezes(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertTrue(self.rh.remover(self.cpfAlessio), "Deve ser possivel remover funcionario cadastrado")
         self.assertFalse(self.rh.remover(self.cpfAlessio), "Nao e possivel remover um usuario duas vezes")
         self.assertEqual(5, self.rh.getTotalFuncionarios(), "O total de funcionarios deve ser 5")
 
     def test_removerFuncionarioInexistente(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertFalse(self.rh.remover(self.cpfNulo), "Nao e possivel remover um usuario nao cadastrado")
         self.assertEqual(6, self.rh.getTotalFuncionarios(), "O total de funcionarios deve ser 6")
 
     def test_buscarProfessor(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
         self.assertEqual(self.profJonas, self.rh.obterFuncionario(self.cpfJonas), "Deve ser possivel achar esse professor")
 
     def test_buscarSTA(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
         self.assertEqual(self.staMiriam, self.rh.obterFuncionario(self.cpfMiriam), "Deve ser possivel achar esse STA")
 
     def buscarTerceirizado(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
         self.assertEqual(self.tercCarla, self.rh.obterFuncionario(self.cpfCarla),
                          "Deve ser possivel achar esse terceirizado")
 
     def buscarFuncionariosNaoExistente(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertTrue(self.rh.remover(self.cpfLacerda))
         self.assertTrue(self.rh.remover(self.cpfAlessio))
@@ -116,7 +116,7 @@ class RHServiceCase(unittest.TestCase):
         self.assertEqual(None, self.rh.obterFuncionario(self.cpfLacerda), "Este funcionario foi removido antes")
 
     def test_buscarTodosOsProfessores(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         profChico = Professor("91", "Chico", 'E')
         profX = Professor("92", "Xarles", 'D')
@@ -128,7 +128,7 @@ class RHServiceCase(unittest.TestCase):
                          "A lista deve conter os mesmo funcionario e deve estar ordenada pelo nome funcionario")
 
     def test_buscarTodosOsSTAs(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
         profChico = Professor("91", "Chico", 'E')
         profX = Professor("92", "Xarles", 'D')
         self.rh.cadastrar(profChico)
@@ -139,14 +139,14 @@ class RHServiceCase(unittest.TestCase):
                          "A lista deve conter os mesmo funcionario e deve estar ordenada pelo nome funcionario")
 
     def test_buscarTodosOsTerceirizados(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertEqual([self.tercAdriana, self.tercCarla],
                          self.rh.getFuncionariosPorCategoria(IRHService.Tipo.TERC),
                          "A lista deve conter os mesmo funcionario e deve estar ordenada pelo nome funcionario")
 
     def test_buscarTodosOsFuncionarios(self):
-        self.inserirFuncionarios()
+        self.test_inserirFuncionarios()
 
         self.assertEqual(
             [self.tercAdriana, self.profAlessio, self.tercCarla, self.profJonas, self.staLacerda,
@@ -158,67 +158,65 @@ class RHServiceCase(unittest.TestCase):
         self.assertTrue(self.rh.cadastrar(self.profJonas))
         self.assertTrue(self.rh.cadastrar(self.profAlessio))
 
-        self.assertEqual(7000.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01, "Calculo incorreto")
-        self.assertEqual(5000.0, self.rh.calcularSalarioDoFuncionario(self.cpfAlessio), 0.01, "Calculo incorreto")
+        self.assertEqual(7000.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), "Calculo incorreto")
+        self.assertEqual(5000.0, self.rh.calcularSalarioDoFuncionario(self.cpfAlessio), "Calculo incorreto")
 
     def test_calcularSalarioSTA(self):
         self.assertTrue(self.rh.cadastrar(self.staMiriam))
-        self.assertEqual(2000.0, self.rh.calcularSalarioDoFuncionario(self.cpfMiriam), 0.01, "Calculo incorreto")
+        self.assertEqual(2000.0, self.rh.calcularSalarioDoFuncionario(self.cpfMiriam), "Calculo incorreto")
 
     def test_calcularSalarioTerceirizados(self):
         self.assertTrue(self.rh.cadastrar(self.tercCarla))
-        self.assertEqual(1000.0, self.rh.calcularSalarioDoFuncionario(self.cpfCarla), 0.01, "Calculo incorreto")
+        self.assertEqual(1000.0, self.rh.calcularSalarioDoFuncionario(self.cpfCarla),"Calculo incorreto")
 
     def test_salarioProfessorComDiaria(self):
         self.assertTrue(self.rh.cadastrar(self.profJonas))
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas), "Um professor tem direito a tres diarias")
-        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01, "Calculo de diaria incorreto")
+        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), "Calculo de diaria incorreto")
 
     def test_salarioSTAComDiaria(self):
         self.assertTrue(self.rh.cadastrar(self.staLacerda))
         self.assertTrue(self.rh.solicitarDiaria(self.cpfLacerda), "Um sta tem direito a uma diaria")
-        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda), 0.01, "Calculo de diaria incorreto")
+        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda), "Calculo de diaria incorreto")
 
     def test_salarioTerceirizadoComDiaria(self):
         self.assertTrue(self.rh.cadastrar(self.tercCarla))
         self.assertFalse(self.rh.solicitarDiaria(self.cpfCarla), "Um terceirizado nao tem direito a diaria")
-        self.assertEqual(1000.0, self.rh.calcularSalarioDoFuncionario(self.cpfCarla), 0.01, "Calculo de diarias incorreto")
+        self.assertEqual(1000.0, self.rh.calcularSalarioDoFuncionario(self.cpfCarla), "Calculo de diarias incorreto")
 
     def test_diariaAlemDoLimiteProfessor(self):
         self.assertTrue(self.rh.cadastrar(self.profJonas))
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas), "Um professor tem direito a tres diarias")
-        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01,
-                         "O calculo do salario esta incorreto")
+        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), "O calculo do salario esta incorreto")
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas), "Um professor tem direito a tres diarias")
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas), "Um professor tem direito a tres diarias")
         self.assertFalse(self.rh.solicitarDiaria(self.cpfJonas), "Diarias alem do limite foram concedidas")
-        self.assertEqual(7300.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01,
+        self.assertEqual(7300.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas),
                          "O calculo do salario esta incorreto")
 
     def test_diariaAlemDoLimiteSTA(self):
         self.assertTrue(self.rh.cadastrar(self.staLacerda))
         self.assertTrue(self.rh.solicitarDiaria(self.cpfLacerda))
-        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda), 0.01,
+        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda),
                          "O calculo do salario esta incorreto")
         self.assertFalse(self.rh.solicitarDiaria(self.cpfLacerda), "O funcionario nao tem mais direito a diarias")
-        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda), 0.01,
-                         "O calculo do salario esta incorreto")
+        self.assertEqual(1600.0, self.rh.calcularSalarioDoFuncionario(self.cpfLacerda), "O calculo do salario esta incorreto")
 
     def test_calcularFolhaVazia(self):
-        self.assertEqual(0.0, self.rh.calcularFolhaDePagamento(), 0.01, "Folha de pagamento esta vazia")
+        self.assertEqual(0.0, self.rh.calcularFolhaDePagamento(), "Folha de pagamento esta vazia")
 
     def test_calcularFolha(self):
         self.assertTrue(self.rh.cadastrar(self.profJonas))
         self.assertTrue(self.rh.cadastrar(self.staMiriam))
         self.assertTrue(self.rh.cadastrar(self.tercCarla))
 
-        self.assertEqual(10000.0, self.rh.calcularFolhaDePagamento(), 0.01, "Soma de salarios incorreta")
+        self.assertEqual(10000.0, self.rh.calcularFolhaDePagamento(), "Soma de salarios incorreta")
 
         self.assertTrue(self.rh.cadastrar(self.staLacerda))
         self.assertTrue(self.rh.cadastrar(self.profAlessio))
         self.assertTrue(self.rh.cadastrar(self.tercAdriana))
 
-        self.assertEqual(18000.0, self.rh.calcularFolhaDePagamento(), 0.01, "Soma de salarios incorreta")
+        self.assertEqual(18000.0, self.rh.calcularFolhaDePagamento(),"Soma de salarios incorreta")
 
     def test_calcularFolhaComDiarias(self):
         self.assertEqual(0.0, self.rh.calcularFolhaDePagamento(), 0.01)
@@ -227,30 +225,28 @@ class RHServiceCase(unittest.TestCase):
         self.assertTrue(self.rh.cadastrar(self.staMiriam))
         self.assertTrue(self.rh.cadastrar(self.tercCarla))
 
-        self.assertEqual(10000.0, self.rh.calcularFolhaDePagamento(), 0.01, "Soma de salarios incorreta")
+        self.assertEqual(10000.0, self.rh.calcularFolhaDePagamento(), "Soma de salarios incorreta")
 
         self.assertTrue(self.rh.cadastrar(self.staLacerda))
         self.assertTrue(self.rh.cadastrar(self.profAlessio))
         self.assertTrue(self.rh.cadastrar(self.tercAdriana))
 
-        self.assertEqual(18000.0, self.rh.calcularFolhaDePagamento(), 0.01, "Soma de salarios incorreta")
+        self.assertEqual(18000.0, self.rh.calcularFolhaDePagamento(), "Soma de salarios incorreta")
 
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas))
-        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01,
-                         "Soma de salarios com diaria incorreta")
+        self.assertEqual(7100.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas),"Soma de salarios com diaria incorreta")
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas))
         self.assertTrue(self.rh.solicitarDiaria(self.cpfJonas))
         self.assertFalse(self.rh.solicitarDiaria(self.cpfJonas))
 
-        self.assertEqual(7300.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas), 0.01,
-                         "Soma de salarios com diaria incorreta")
+        self.assertEqual(7300.0, self.rh.calcularSalarioDoFuncionario(self.cpfJonas),"Soma de salarios com diaria incorreta")
 
         self.assertFalse(self.rh.solicitarDiaria(self.cpfCarla))
 
         self.assertTrue(self.rh.solicitarDiaria(self.cpfLacerda))
         self.assertFalse(self.rh.solicitarDiaria(self.cpfLacerda))
 
-        self.assertEqual(18400.0, self.rh.calcularFolhaDePagamento(), 0.01, "Soma de salarios com diaria incorreta")
+        self.assertEqual(18400.0, self.rh.calcularFolhaDePagamento(), "Soma de salarios com diaria incorreta")
 
     def test_participacaoNosLucros(self):
         self.assertEquals(0.0, self.rh.calcularFolhaDePagamento(), 0.01)
@@ -263,16 +259,16 @@ class RHServiceCase(unittest.TestCase):
 
         self.assertTrue(self.rh.partilharLucros(6.00))
 
-        self.assertEquals(2002.0, self.rh.calcularSalarioDoFuncionario(self.cpfMiriam), 0.01,
+        self.assertEquals(2002.0, self.rh.calcularSalarioDoFuncionario(self.cpfMiriam),
                           "Salarios com participacao nos lucros incorreto")
 
     def test_calcularFolhaComPL(self):
-        self.participacaoNosLucros()
-        self.assertEqual(10006.0, self.rh.calcularFolhaDePagamento(), 0.01,
+        self.test_participacaoNosLucros()
+        self.assertEqual(10006.0, self.rh.calcularFolhaDePagamento(),
                          "Soma de salarios com participacao nos lucros incorreta")
 
     def test_iniciandoNovoMes(self):
-        self.calcularFolhaComPL()
+        self.test_calcularFolhaComPL()
         self.rh.iniciarMes()
         self.assertEqual(10000.0, self.rh.calcularFolhaDePagamento(), 0.01)
 
